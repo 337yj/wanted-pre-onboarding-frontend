7a58854c20c86cb9510db7b32b113fa8b3827548
337yj <337yje@gmail.com>
2023-08-05 13:38:42 +0900

    feat : 로그아웃 구현

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,13 +1,24 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
+import useAuthContext from "../../../context/AuthContext";
 import styles from "./header.module.scss";
 
 const Header = () => {
+  const { token, clearToken } = useContext(useAuthContext);
+
+  const logout = () => {
+    clearToken();
+  };
+
   return (
     <header>
-      <div className={styles.wrapper}>
-        <h1>ToDo List</h1>
-        <button type="button">로그아웃</button>
-      </div>
+      {token !== null && (
+        <div className={styles.wrapper}>
+          <h1>ToDo List</h1>
+          <button type="button" onClick={logout}>
+            로그아웃
+          </button>
+        </div>
+      )}
     </header>
   );
 };
